0688e99d43b355115f5f30a3719a65c874456333
TolaMironcenko <mironcenkotola@gmail.com>
2022-09-28 23:35:05 +0700

    initial commit

---
+++ in.h
@@ -0,0 +1,15 @@
+#ifndef IN_H
+#define IN_H
+
+struct sockaddr_in {
+    short            sin_family;   // e.g. AF_INET
+    unsigned short   sin_port;     // e.g. htons(3490)
+    struct in_addr   sin_addr;     // see struct in_addr, below
+    char             sin_zero[8];  // zero this if you want to
+};
+
+struct in_addr {
+    unsigned long s_addr;  // load with inet_aton()
+};
+
+#endif // IN_H
