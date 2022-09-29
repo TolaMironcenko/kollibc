5dabcdf3bf1c6eacc4131635d6fa6191467a948a
TolaMironcenko <mironcenkotola@gmail.com>
2022-09-29 10:04:18 +0700

    commit

--- examples/main.c
+++ examples/main.c
@@ -14,9 +14,15 @@ int main()
 
 	inet_ntop(AF_INET, &(server_addr.sin_addr), inet_addr_to_terminal, sizeof(server_addr));
 
-	printf("sin_family = %d\nsin_port = %d\nsin_addr = %s\n", server_addr.sin_family, htons(server_addr.sin_port), inet_addr_to_terminal);
+	printf("sin_family = %d\nsin_port = %d\nsin_addr = %s\n",
+           server_addr.sin_family,
+           htons(server_addr.sin_port),
+           inet_addr_to_terminal);
 
-	printf("sin_familyd = %d\nsin_port_uns_short = %d\nsin_addrd = %ld\n", server_addr.sin_family, server_addr.sin_port, server_addr.sin_addr.s_addr);
+	printf("sin_familyd = %d\nsin_port_uns_short = %d\nsin_addrd = %ld\n",
+           server_addr.sin_family,
+           server_addr.sin_port,
+           server_addr.sin_addr.s_addr);
 
 	return 0;
 }
