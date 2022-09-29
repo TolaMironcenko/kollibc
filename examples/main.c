17556efc37551375bf785e151838ca92484c7f7c
TolaMironcenko <mironcenkotola@gmail.com>
2022-09-29 09:50:17 +0700

    examples and added structs and some functions for kolibrios but not tested

---
+++ examples/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "../src/in.h"
+#include <sys/socket.h>
+
+int main()
+{
+	struct sockaddr_in server_addr;
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(9090);
+	inet_aton("127.0.0.3", &server_addr.sin_addr);
+
+	char inet_addr_to_terminal[sizeof(server_addr.sin_addr)];
+
+	inet_ntop(AF_INET, &server_addr.sin_addr, inet_addr_to_terminal, sizeof(server_addr));
+
+	printf("sin_family = %d\nsin_port = %d\nsin_addr = %s\n", server_addr.sin_family, htons(server_addr.sin_port), inet_addr_to_terminal);
+
+	printf("sin_familyd = %d\nsin_port_uns_short = %d\nsin_addrd = %ld\n", server_addr.sin_family, server_addr.sin_port, server_addr.sin_addr.s_addr);
+
+	return 0;
+}
--- src/in.h
+++ src/in.h
@@ -1,6 +1,10 @@
 #ifndef IN_H
 #define IN_H
 
+struct in_addr {
+    unsigned long s_addr;  // load with inet_aton()
+};
+
 struct sockaddr_in {
     short            sin_family;   // e.g. AF_INET
     unsigned short   sin_port;     // e.g. htons(3490)
@@ -8,8 +12,11 @@ struct sockaddr_in {
     char             sin_zero[8];  // zero this if you want to
 };
 
-struct in_addr {
-    unsigned long s_addr;  // load with inet_aton()
-};
+unsigned short htons(unsigned short a);
+
+
+int inet_aton(const char *cp, struct in_addr *inp);
+const char *inet_ntop(int af, const void *restrict src,
+                             char *restrict dst, int size);
 
 #endif // IN_H
